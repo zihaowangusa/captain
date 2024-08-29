67e8c08176c119cf16630561ededab5e8b6b0c8a
zihaowangusa <50635536+zihaowangusa@users.noreply.github.com>
2024-08-30 00:29:56 +0800

    Delete dist directory

--- dist/assets/dashboard-zWlfC6us.js
+++
@@ -1,7 +0,0 @@
-import { j as jsxRuntimeExports } from "./index-CnDKNMMk.js";
-const Dashboard = () => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: "Welcome to Captain Dental" });
-};
-export {
-  Dashboard as default
-};
